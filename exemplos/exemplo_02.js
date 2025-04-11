53482fa2fbf20202992949d2072fab29b1476cf7
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-04-11 09:13:01 -0300

    Exemplos

---
+++ exemplos/exemplo_02.js
@@ -0,0 +1,6 @@
+let tempo = 10;
+
+while (tempo >= 0){
+    console.log(`Contagem Regressiva ${tempo}`);
+    tempo--;
+}
---
+++ exemplos/exemplo_03.js
@@ -0,0 +1,10 @@
+let numero = 1;
+
+while (numero <= 10){
+    console.log(numero);
+    if(numero === 3){""
+        console.log("Parando no 3!");
+        break;
+    }
+    numero++
+}
