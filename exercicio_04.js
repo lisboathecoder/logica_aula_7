2aa75c0aba34a95189a7cf1ffc901a5d2edc3ba7
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-04-11 10:50:55 -0300

    feat(Tabuada do 5)

--- exercicio_04.js
+++ exercicio_04.js
@@ -1,8 +1,8 @@
-let numero = 1
+let numero = 1;
 let calculo;
 
 while (numero <= 10) {
-    calculo = numero * 5
-    console.log(`${numero} x 5 = ${calculo}`);
-    numero++
+  calculo = numero * 5;
+  console.log(`${numero} x 5 = ${calculo}`);
+  numero++;
 }
